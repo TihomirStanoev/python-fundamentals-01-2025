18b8be9e0693b4231741848d656c60d2dd364487
Tihomir Stanoev <tstanoev@yandex.com>
2025-01-21 01:59:50 +0200

    Update

--- Personal Fitness Tracker System/personal_fitness_tracker_system.py
+++ Personal Fitness Tracker System/personal_fitness_tracker_system.py
@@ -11,7 +11,8 @@ calories = []  # To store calorie intake for meals
 workout_goal = 0  # Daily workout goal in minutes
 calorie_goal = 0  # Daily calorie intake goal
 
-def calculations(data_list, parameter): # First argument list, second str 'workout'/'meal'
+
+def calculations(data_list, parameter):  # First argument list, second str 'workout'/'meal'
     """
     Calculations.
     - Calculate all data needed for progres.
@@ -21,17 +22,16 @@ def calculations(data_list, parameter): # First argument list, second str 'worko
     unit = ' '
     # Check header of table
     if parameter == 'workout':
-        table = '🏋️ Workout  ⏱️ \n' # Table header if parameter is 'workout'
+        table = '🏋️ Workout  ⏱️ \n'  # Table header if parameter is 'workout'
         unit = ' min'
     elif parameter == 'meal':
-        table = '🥓️ Meal    ⚡⚡⚡\n' # Table header if parameter is 'meal'
+        table = '🥓️ Meal    ⚡⚡⚡\n'  # Table header if parameter is 'meal'
         unit = ' cal'
 
-
-    count = 0 # Count str values
-    total = 0 # Sum of numeric values
-    best_list = [] # List with two items
-    best, best_index = 0, 0 # 'best' is the highest value in list, best_index is his index or index-1 is TEXT of highest value
+    count = 0  # Count str values
+    total = 0  # Sum of numeric values
+    best_list = []  # List with two items
+    best, best_index = 0, 0  # 'best' is the highest value in list, best_index is his index or index-1 is TEXT of highest value
 
     #Loop for separate workouts/meals and duration/calories
     for index in range(len(data_list)):
@@ -53,11 +53,10 @@ def calculations(data_list, parameter): # First argument list, second str 'worko
     best_list.append(data_list[best_index - 1])
     best_list.append(data_list[best_index])
 
-
     return table, count, total, best_list
 
 
-def log_workout(workout_type, duration): # Input srt, int
+def log_workout(workout_type, duration):  # Input srt, int
     """
     Log a workout.
     - Append the workout type and duration to the workouts list.
@@ -76,18 +75,19 @@ def log_workout(workout_type, duration): # Input srt, int
         print("The workout is not saved!")
     print()
 
-def log_calorie_intake(calories_consumed): # !Input list
+
+def log_calorie_intake(calories_consumed):  # !Input list
     """
     Log calorie intake for a meal.
     - Append the calorie amount to the calories list.
     - Print a confirmation message.
     """
-    calculation_type = 'meal' # Type for calculation, only used for UoM and table header
+    calculation_type = 'meal'  # Type for calculation, only used for UoM and table header
 
     meals = calculations(calories_consumed, calculation_type)
-    meals_table = meals[0] # Table
-    meals_count = meals[1] # Count of meals
-    calorie_total = meals[2] # Sum of calories
+    meals_table = meals[0]  # Table
+    meals_count = meals[1]  # Count of meals
+    calorie_total = meals[2]  # Sum of calories
 
     print(f'Do you want to save {meals_count} meals with total {calorie_total} calories:\n')
     print(f'{meals_table}')
@@ -101,7 +101,6 @@ def log_calorie_intake(calories_consumed): # !Input list
         print("The meals are not saved!\n")
 
 
-
 def view_progress(choose):
     """
     Display a summary of the user's progress for the day.
@@ -110,22 +109,19 @@ def view_progress(choose):
     """
     # Calories, Workout, Summary
 
-
     if choose == '1' or choose == '3':
         # Check for recorded workout.
         if not workouts:
             return print('You don\'t have any workouts recorded yet')
 
-
-        calculation_type = 'workout' # Set calculations for workouts
+        calculation_type = 'workout'  # Set calculations for workouts
         workout_table, workouts_count, total_duration, longest_workout = calculations(workouts, calculation_type)
-
 
         # Printing summary info for workouts
         print('📊 Let\'s see a summary view of all you\'r workouts.\n')
         print('▫️All your training until the moment:\n' + workout_table)
         print(f'▫️So far you have {workouts_count} workouts with a total duration {total_duration} mins.')
-        print(f'▫️Average duration per workout: {total_duration/workouts_count:.0f} min.')
+        print(f'▫️Average duration per workout: {total_duration / workouts_count:.0f} min.')
         print(f'▫️Yo\'re best workout is {longest_workout[0].lower()} with {longest_workout[1]} min duration!\n')
         input("Press ENTER to continue..")
 
@@ -133,13 +129,13 @@ def view_progress(choose):
         if not calories:
             return print('You don\'t have any melas recorded yet')
 
-        calculation_type = 'meal' # Set calculations for calories
+        calculation_type = 'meal'  # Set calculations for calories
         meal_table, meals_count, total_calories, most_calorie = calculations(calories, calculation_type)
 
-        print(meal_table) #test
-        print(f'Total meals: {meals_count}') #test
-        print(f'Total cals: {total_calories}') #test
-        print(f'Top meal: {most_calorie[0]} - {most_calorie[1]} cal') #test
+        print(meal_table)  #test
+        print(f'Total meals: {meals_count}')  #test
+        print(f'Total cals: {total_calories}')  #test
+        print(f'Top meal: {most_calorie[0]} - {most_calorie[1]} cal')  #test
         input("Press ENTER to continue..")
 
 
@@ -175,11 +171,9 @@ def main():
     """
     print("Welcome to the Personal Fitness Tracker System 🏋️‍♂️\n")
 
-
-
     while True:
         # Print workouts list to check for working
-#        print(workouts) # print workouts just for "log workout" test.
+        #        print(workouts) # print workouts just for "log workout" test.
 
         # Display menu options
         print("1. Log Workout")
@@ -197,13 +191,14 @@ def main():
             type_of_workout = ''
             duration = 0
             while True:
-                workout_choose = input('Choose a workout from the menu, you can save your own workout by choosing "Other":\n\n'
-                      '\t1. 🏃‍♂️ Running\n'
-                      '\t2. 🚲 Cycling\n'
-                      '\t3. 🚶‍♂️ Walking\n'
-                      '\t4. 💪 Fitness\n'
-                      '\t5. Other\n\n'
-                      'You\'r choice: ')
+                workout_choose = input(
+                    'Choose a workout from the menu, you can save your own workout by choosing "Other":\n\n'
+                    '\t1. 🏃‍♂️ Running\n'
+                    '\t2. 🚲 Cycling\n'
+                    '\t3. 🚶‍♂️ Walking\n'
+                    '\t4. 💪 Fitness\n'
+                    '\t5. Other\n\n'
+                    'You\'r choice: ')
 
                 if workout_choose == '1':
                     type_of_workout = 'Running'
@@ -227,11 +222,11 @@ def main():
                     duration = duration_choose
                     break
                 else:
-                    print(f'Invalid Duration {duration_choose} min. The duration must be between 1 and {MAX_DURATION} minutes. ')
+                    print(
+                        f'Invalid Duration {duration_choose} min. The duration must be between 1 and {MAX_DURATION} minutes. ')
                     continue
 
             log_workout(type_of_workout, duration)
-
 
             pass
         elif choice == '2':
@@ -239,7 +234,7 @@ def main():
             print("Add your meals to the moment. The meals should be in a format, for example 150g chicken,\n "
                   "the program will calculate the calories itself. [Input \'e\' for exit..]\n Enter a meal: ", end='')
 
-            total_meals = [] # List with meals for save
+            total_meals = []  # List with meals for save
 
             while True:
                 meal_input = input().lower()
@@ -247,13 +242,13 @@ def main():
                 if meal_input == 's' or meal_input == 'e':
                     break
 
-                meal_list = nutri_list(meal_input) # Get calories
+                meal_list = nutri_list(meal_input)  # Get calories
 
                 if meal_list == 'error':
                     print("Wrong input, try again with correct meal: ", end='')
                     continue
 
-                for index in range(len(meal_list)): # Print and append elements from temp list
+                for index in range(len(meal_list)):  # Print and append elements from temp list
                     if index % 2 == 0:
                         meal_name = meal_list[index].title()
                         print(f'{meal_name} is', end=' ')
@@ -263,13 +258,12 @@ def main():
                         print(f'{cals} calories, add other meals or input \'s\' for save: ', end='')
                         total_meals.append(cals)
 
-
             print('\n\n')
 
-            if not total_meals or meal_input == 'e': # Continue if first input = 's'
+            if not total_meals or meal_input == 'e':  # Continue if first input = 's'
                 continue
             else:
-                log_calorie_intake(total_meals) # Send a list for a calculations before being saved.
+                log_calorie_intake(total_meals)  # Send a list for a calculations before being saved.
 
 
         elif choice == '3':
